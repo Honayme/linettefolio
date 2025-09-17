847add5aace698a9cb119ee9800d5f1cc943e61d
Thomas <thomas.mirabile@ynov.com>
2025-09-17 03:24:13 +0200

    Change contact section

--- resources/views/livewire/contact.blade.php
+++ resources/views/livewire/contact.blade.php
@@ -14,59 +14,76 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="map_wrap w-full float-left clear-both h-auto mb-[50px]">
-                            <div class="mapouter">
-                                <div class="gmap_canvas">
-                                    <iframe width="100%" height="355" id="gmap_canvas"
-                                            src="https://maps.google.com/maps?q=2880%20Broadway,%20New%20York&t=&z=13&ie=UTF8&iwloc=&output=embed"
-                                            frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
-                                    <a href="https://fmovies-online.net">fmovies</a><br>
-                                    <style>.mapouter {
-                                            position: relative;
-                                            text-align: right;
-                                            height: 355px;
-                                            width: 100%;
-                                        }</style>
-                                    <a href="https://www.embedgooglemap.net">embedgooglemap.net</a>
-                                    <style>.gmap_canvas {
-                                            overflow: hidden;
-                                            background: none !important;
-                                            height: 355px;
-                                            width: 100%;
-                                        }</style>
+                        <div class="map_wrap w-full float-left clear-both h-auto mb-[60px]">
+                            <div class="bg-white rounded-2xl p-4 shadow-sm">
+                                <div class="mapouter">
+                                    <div class="gmap_canvas">
+                                        <iframe width="100%" height="500" id="gmap_canvas"
+                                                src="https://maps.google.com/maps?q=88c%20rue%20de%20bas%20vernaz,%2074240%20GAILLARD&t=&z=15&ie=UTF8&iwloc=&output=embed"
+                                                frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
+                                                class="rounded-xl w-full"></iframe>
+                                        <style>.mapouter {
+                                                position: relative;
+                                                text-align: right;
+                                                height: 500px;
+                                                width: 100%;
+                                            }</style>
+                                        <style>.gmap_canvas {
+                                                overflow: hidden;
+                                                background: none !important;
+                                                height: 500px;
+                                                width: 100%;
+                                            }</style>
+                                    </div>
                                 </div>
                             </div>
-
-                            <!-- Get your API here https://www.embedgooglemap.net -->
-
                         </div>
-                        <div class="fields w-full float-left clear-both h-auto">
-                            <form action="/" method="post" class="contact_form" id="contact_form" autocomplete="off">
-                                <div class="returnmessage"
-                                     data-success="Your message has been received, We will contact you soon."></div>
-                                <div class="empty_notice"><span>Please Fill Required Fields</span></div>
-                                <div class="first w-full float-left">
-                                    <ul class="list-none">
-                                        <li class="w-full mb-[30px] float-left">
-                                            <input id="name" type="text" placeholder="Name">
-                                        </li>
-                                        <li class="w-full mb-[30px] float-left">
-                                            <input id="email" type="text" placeholder="Email">
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div class="last">
-                                    <textarea id="message" placeholder="Message"></textarea>
-                                </div>
-                                <div class="tokyo_tm_button" data-position="left">
-                                    <a id="send_message" href="#">
-                                        <span>Send Message</span>
+                        <!-- Section des informations de contact -->
+                        <div class="contact_info w-full float-left clear-both h-auto">
+                            <div class="grid grid-cols-1 md:grid-cols-3 gap-8 text-center">
+
+                                <!-- Email -->
+                                <div class="contact_item">
+                                    <div class="w-16 h-16 bg-orange rounded-full flex items-center justify-center mx-auto mb-4">
+                                        <svg class="w-8 h-8 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 4.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/>
+                                        </svg>
+                                    </div>
+                                    <h4 class="text-lg font-bold text-black mb-2">Email</h4>
+                                    <a href="mailto:lina-marie.michel@hotmail.fr" class="text-gray-700 text-base hover:text-black transition-colors font-medium">
+                                        lina-marie.michel@hotmail.fr
                                     </a>
                                 </div>
 
-                                <!-- If you want to change mail address to yours, please open modal.php and go to line 4 -->
+                                <!-- Téléphone -->
+                                <div class="contact_item">
+                                    <div class="w-16 h-16 bg-orange rounded-full flex items-center justify-center mx-auto mb-4">
+                                        <svg class="w-8 h-8 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"/>
+                                        </svg>
+                                    </div>
+                                    <h4 class="text-lg font-bold text-black mb-2">Téléphone</h4>
+                                    <a href="tel:+33605276622" class="text-gray-700 text-base hover:text-black transition-colors font-medium">
+                                        +33 6 05 27 66 22
+                                    </a>
+                                </div>
 
-                            </form>
+                                <!-- Adresse -->
+                                <div class="contact_item">
+                                    <div class="w-16 h-16 bg-orange rounded-full flex items-center justify-center mx-auto mb-4">
+                                        <svg class="w-8 h-8 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"/>
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"/>
+                                        </svg>
+                                    </div>
+                                    <h4 class="text-lg font-bold text-black mb-2">Adresse</h4>
+                                    <p class="text-gray-700 text-base font-medium">
+                                        88c rue de bas vernaz<br>
+                                        74240 GAILLARD
+                                    </p>
+                                </div>
+
+                            </div>
                         </div>
                     </div>
                 </div>
