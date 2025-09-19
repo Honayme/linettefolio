051d489f8186d7e61e55c526631c111a8972944d
Thomas <thomas.mirabile@ynov.com>
2025-09-20 01:16:14 +0200

    repair service section

--- app/Filament/Resources/ServiceResource/Pages/CreateService.php
+++ app/Filament/Resources/ServiceResource/Pages/CreateService.php
@@ -3,11 +3,7 @@
 namespace App\Filament\Resources\ServiceResource\Pages;
 
 use App\Filament\Resources\ServiceResource;
-use Filament\Actions;
-use Filament\Actions\Action;
 use Filament\Resources\Pages\CreateRecord;
-use Illuminate\Support\HtmlString;
-use Illuminate\Support\Str;
 
 class CreateService extends CreateRecord
 {
@@ -15,35 +11,4 @@ class CreateService extends CreateRecord
 
 
 
-    // On ajoute la même méthode ici
-    protected function getHeaderActions(): array
-    {
-        return [
-            Action::make('preview')
-                ->label('Aperçu')
-                ->icon('heroicon-o-eye')
-                ->color('gray')
-                ->modalContent(function (Action $action) {
-                    $data = $action->getForm()->getState();
-                    $markdown = $data['full_content'] ?? null;
-
-                    if (!$markdown) {
-                        return new HtmlString('<div class="text-gray-500">Commencez à écrire pour voir un aperçu.</div>');
-                    }
-
-                    $html = Str::markdown($markdown);
-                    return new HtmlString($html);
-                })
-                ->modalHeading('Aperçu du contenu')
-                ->modalSubmitAction(false)
-                ->modalCancelActionLabel('Fermer'),
-        ];
-    }
-
-    protected function getForms(): array
-    {
-        return [
-            'form' => $this->form->model($this->getModel())->statePath('data')->live(),
-        ];
-    }
 }
