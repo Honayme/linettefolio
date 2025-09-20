4aca7ce4750e8335351ccf2e773f997a21b0b0df
Thomas <thomas.mirabile@ynov.com>
2025-09-20 06:37:31 +0200

    Delete useless javascript

--- resources/js/app.js
+++ resources/js/app.js
@@ -168,27 +168,6 @@ Alpine.data('pdfViewerOverlay', () => ({
 // Maintenant que tout est configuré, démarrer Livewire (qui démarrera Alpine)
 Livewire.start();
 
-// Attend que le DOM soit entièrement chargé avant d'exécuter le code.
-/*
-$(function() {
-    console.log("DOM is ready. Loading plugins first...");
-
-    // 1. On charge d'abord plugins.js
-    import('../../public/template/plugins.js')
-        .then(() => {
-            // 2. On utilise setTimeout pour pousser l'exécution de init.js
-            //    à la fin de la file d'attente du navigateur.
-            //    Cela garantit que TOUS les plugins ont eu le temps de s'initialiser.
-            setTimeout(() => {
-                console.log("Plugins should be fully initialized. Now loading init script.");
-                import('../../public/template/init.js');
-            }, 0); // Le délai de 0ms est crucial ici
-        })
-        .catch(error => {
-            console.error("Error loading template scripts:", error);
-        });
-});
-*/
 
 // La version améliorée avec la délégation d'événements
 function tokyo_tm_trigger_menu_delegated(){
